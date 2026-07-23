ff596b3c9eac2dd242d5aee4cc7a5e166c5a151c
Ted W. <ted.l.wood@gmail.com>
2026-07-23 13:03:16 -0400

    feat: implement refresh subcommand

--- cmd/refresh.go
+++ cmd/refresh.go
@@ -1,3 +1,38 @@
 package cmd
 
-func runRefresh() {}
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+func runRefresh() {
+	pidPath := sharedPIDPath()
+	data, err := os.ReadFile(pidPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "refresh: daemon not running (no PID file at %s)\n", pidPath)
+		os.Exit(1)
+	}
+
+	pidStr := strings.TrimSpace(string(data))
+	pid, err := strconv.Atoi(pidStr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "refresh: invalid PID %q\n", pidStr)
+		os.Exit(1)
+	}
+
+	proc, err := os.FindProcess(pid)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "refresh: process %d not found\n", pid)
+		os.Exit(1)
+	}
+
+	if err := proc.Signal(syscall.SIGUSR1); err != nil {
+		fmt.Fprintf(os.Stderr, "refresh: signal %d: %v\n", pid, err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("refresh: signaled daemon (PID %d)\n", pid)
+}
